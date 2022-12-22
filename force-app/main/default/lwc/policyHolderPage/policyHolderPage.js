2d0621746c454bf8f14cb0decfef07d2cea04db5
durgarao <durgaraovejju@gmail.com>
2022-12-22 18:23:23 +0530

    phFinal

--- force-app/main/default/lwc/policyHolderPage/policyHolderPage.js
+++ force-app/main/default/lwc/policyHolderPage/policyHolderPage.js
@@ -1,6 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,wire,track,api } from 'lwc';
+import lifeData1 from '@salesforce/apex/homeclass.lifeData'
+import MotorData1 from '@salesforce/apex/homeclass.MotorData'
 
 export default class PolicyHolderPage extends LightningElement {
-    handle(){}
-    submitHandle(){}
+    @track showdata=true;
+    @track phId=null
+    @track lifeType='Life Insurence'
+    @track motorType='Motor Insurnce'
+    @track lifeData =null;
+    @track MotorData =null;
+    handle(event){
+        this.phId=event.target.value;
+        console.log(this.phId)
+    }
+    @wire(lifeData1,{data:'$phId'})data12({data,error}){
+        if(data){
+            console.log(data)
+        }
+    }
+    submitHandle(){
+    
+    
+    
+        lifeData1({data:this.phId}).then(result=>{
+            this.lifeData=result;
+            console.log(result);
+        }).catch(error=>{
+            console.error(error)
+        });
+       
+        MotorData1({data:this.phId}).then(result=>{
+          this.MotorData=result;
+          console.log(result);
+        }).catch(error=>{
+            console.error(error);
+        });
+
+    }
 }
